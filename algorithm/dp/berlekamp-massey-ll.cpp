221b56f3bcc253420e8f78aac3fb094932c7fe0d
Kcits970 <geometricstick@gmail.com>
2024-11-20 14:00:13 +0900

    added file extension to berlekamp-massey file

---
+++ algorithm/dp/berlekamp-massey-ll.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <tuple>
+using namespace std;
+using ll = long long;
+
+const ll mod = 1000000007;
+
+ll ipow(ll x, ll p) {
+	ll ret = 1, piv = x;
+	while (p) {
+		if (p & 1) ret = ret * piv % mod;
+		piv = piv * piv % mod;
+		p >>= 1;
+	}
+	return ret;
+}
+vector<ll> berlekamp_massey(vector<ll> x) {
+	vector<ll> ls, cur;
+	ll lf, ld;
+	for (size_t i = 0; i < x.size(); i++) {
+		ll t = 0;
+		for (size_t j = 0; j < cur.size(); j++) {
+			t = (t + 1ll * x[i - j - 1] * cur[j]) % mod;
+		}
+		if ((t - x[i]) % mod == 0) continue;
+		if (cur.empty()) {
+			cur.resize(i + 1);
+			lf = i;
+			ld = (t - x[i]) % mod;
+			continue;
+		}
+		ll k = -(x[i] - t) * ipow(ld, mod - 2) % mod;
+		vector<ll> c(i - lf - 1);
+		c.push_back(k);
+
+		for (auto& j : ls) c.push_back(-j * k % mod);
+
+		if (c.size() < cur.size()) c.resize(cur.size());
+
+		for (size_t j = 0; j < cur.size(); j++) {
+			c[j] = (c[j] + cur[j]) % mod;
+		}
+
+		if (i - lf + (int)ls.size() >= (int)cur.size()) {
+			tie(ls, lf, ld) = make_tuple(cur, i, (t - x[i]) % mod);
+		}
+
+		cur = c;
+	}
+	for (auto& i : cur) i = (i % mod + mod) % mod;
+	return cur;
+}
+ll get_nth(vector<ll> rec, vector<ll> dp, ll n) {
+	ll m = rec.size();
+	vector<ll> s(m), t(m);
+	s[0] = 1;
+	if (m != 1) t[1] = 1;
+	else t[0] = rec[0];
+	auto mul = [&rec](vector<ll> v, vector<ll> w) {
+		int m = v.size();
+		vector<ll> t(2 * m);
+		for (ll j = 0; j < m; j++) {
+			for (ll k = 0; k < m; k++) {
+				t[j + k] += 1ll * v[j] * w[k] % mod;
+				if (t[j + k] >= mod) t[j + k] -= mod;
+			}
+		}
+		for (ll j = 2 * m - 1; j >= m; j--) {
+			for (ll k = 1; k <= m; k++) {
+				t[j - k] += 1ll * t[j] * rec[k - 1] % mod;
+				if (t[j - k] >= mod) t[j - k] -= mod;
+			}
+		}
+		t.resize(m);
+		return t;
+	};
+	while (n) {
+		if (n & 1) s = mul(s, t);
+		t = mul(t, t);
+		n >>= 1;
+	}
+	ll ret = 0;
+	for (ll i = 0; i < m; i++) ret += 1ll * s[i] * dp[i] % mod;
+	return ret % mod;
+}
+
+int guess_nth_term(vector<ll> x, size_t n) {
+	if (n < x.size()) return x[n];
+	vector<ll> v = berlekamp_massey(x);
+	if (v.empty()) return 0;
+	return get_nth(v, x, n);
+}
