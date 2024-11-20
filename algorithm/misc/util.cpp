861fbc021927873707fe231f575e625127e8eb26
amemome <meltksq5140@gmail.com>
2024-11-20 21:07:35 +0900

    네

--- algorithm/misc/util.cpp
+++ algorithm/misc/util.cpp
@@ -43,6 +43,19 @@ struct pos_hash
 
 int main()
 {
+    unsigned seed = chrono::steady_clock::now().time_since_epoch().count();
+    mt19937 rnd(seed);
+    uniform_int_distribution<ll> randInt(1,100);
+    uniform_real_distribution<double> randFlo(0,100);
+    for(int i = 0; i < 100; i++) cout << randInt(rnd) << ' ';
+    for(int i = 0; i < 100; i++) cout << randFlo(rnd) << ' ';
+
+    vector<ll> vec(100);
+    iota(vec.begin(),vec.end(),1); //배열엘 1~100값을 순차적으로 채움
+    for(auto c :vec) cout << c << ' ';
+    shuffle(vec.begin(),vec.end(),rnd); //셔플, 랜덤엔진을 인자로
+    for(auto c :vec) cout << c << ' ';
+    
 	set<pos> test1;
 	unordered_set<pos, pos_hash> test2;
 
